473e73650c9fd1970641da3b5e3f54583c328d82
Mosaad.Ghanem <Mosaad.Ghanem@nicedeer.local>
2025-10-19 09:52:29 +0300

    refactor: sender and reciever can send and recive notifications
     Project1 can send notifications to project2
     Project2 can send notifications to project1
      So we can send notifications between two projects or more like live time chat
      And any project can be sender to one or to all projects or to some specific projects if subscriped to topic
    
     Any project can send notifications to all subscriped in specific topic like follow news letters

--- wwwroot/js/firebase-init.js
+++ wwwroot/js/firebase-init.js
@@ -1,12 +1,22 @@
 // Firebase configuration (replace with your actual Firebase client-side config)
 const firebaseConfig = {
+    ////provider
+    //apiKey: "AIzaSyCm2LHKXITOB99LhN3r2zDXguXoSss1UXA",
+    //authDomain: "notification-test-d2a6e.firebaseapp.com",
+    //projectId: "notification-test-d2a6e",
+    //storageBucket: "notification-test-d2a6e.appspot.com",
+    //messagingSenderId: "266860405652",
+    //appId: "1:266860405652:web:d056b8eb0f509954c42581",
+    //measurementId: "G-XTMFZHS6V5"
+
+    //mg
     apiKey: "AIzaSyA2xHwMSi8S2ZhW0bOUn7P1wTN3Opr4hr8",
     authDomain: "test-notification-94710.firebaseapp.com",
     projectId: "test-notification-94710",
     storageBucket: "test-notification-94710.firebasestorage.app",
     messagingSenderId: "1081156835593",
     appId: "1:1081156835593:web:6681d831783ce25fdedbd8",
-    measurementId: "G-E6LFN9PNK1",
+    measurementId: "G-E6LFN9PNK1"
 };
 
 // Initialize Firebase
